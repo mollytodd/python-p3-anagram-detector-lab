e624ee52a2a6d06dc78f80802a035148eecced99
Molly Todd <mollyjt89@gmail.com>
2023-09-13 14:53:18 -0700

    completed ananagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matched_words = []
+        for word in word_list:
+            if sorted(self.word) == sorted(word):
+                matched_words.append(word)
+        return matched_words if matched_words else []
